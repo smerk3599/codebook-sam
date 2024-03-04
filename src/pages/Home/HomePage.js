bf8177a79d0dfbfc3b9f72f2b780d43f2b096242
Steve Mercer <stevenmercer9913@gmail.com>
2024-03-03 20:47:47 -0800

    Hero Page

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,5 +1,7 @@
 import { Hero } from "./components/Hero";
 import { FeaturedProducts } from "./components/FeaturedProducts";
+import { Testimonials } from "./components/Testimonials";
+import { Faq } from "./components/Faq";
 
 export const HomePage = () => {
   return (
