4e4259506db040a20586e43662953f801fb9afc1
Steve Mercer <stevenmercer9913@gmail.com>
2024-04-16 08:05:39 -0700

    login and register pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export { HomePage } from "./Home/HomePage";
 export { ProductsList } from "./Products/ProductsList";
 export { ProductDetail } from "./ProductDetail";
+export { Register } from "./Register";
+export { Login } from "./Login";
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,13 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import { HomePage, ProductsList, ProductDetail } from "../pages";
+import {
+  Login,
+  Register,
+  HomePage,
+  ProductsList,
+  ProductDetail,
+} from "../pages";
 
 export const AllRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductsList />} />
-        <Route path={"/products/:id"} element={<ProductDetail />} />
+        <Route path="products" element={<ProductsList />} />
+        <Route path={"products/:id"} element={<ProductDetail />} />
+        <Route path={"login"} element={<Login />} />
+        <Route path={"register"} element={<Register />} />
       </Routes>
     </div>
   );
